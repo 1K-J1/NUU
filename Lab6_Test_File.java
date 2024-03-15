ec138da5bc24ada8edd5a27aecd5977efaa8ba04
“Karu <karujuliangiis@gmail.com>
2024-03-15 10:58:57 +0000

    New commit 1

---
+++ Lab6_Test_File.java
@@ -0,0 +1,10 @@
+class Test
+{
+   public static void main (String [] args)
+   {
+     System.out.println("Hello World!");
+     System.out.println("I'm the GOAT!");
+     System.out.println("The question is.. are you? Hmmm.!!!!");
+     return;
+   }
+}
